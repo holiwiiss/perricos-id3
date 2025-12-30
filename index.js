28edb667605ae78e4a6dc2efae6bc0ef3d1d6be3
Paula Ramiro Olivas <paularamiro11@gmail.com>
2025-12-30 20:23:48 +0100

    funcionalidades

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const perricosArray = [];
+const perricosArray = []; //esto era const
+const namePerricosArray = ["Lucas", "Ramon", "Mierdón"]
 
-// addPerrico();
-
-function renderPerricoArray() {
+function renderPerricoArray(perricosList) {
   const dogList = document.querySelector('#dog-list');
   dogList.innerHTML = '';
 
-  perricosArray.forEach((dogImage, index) => {
+  perricosList.forEach((dog, index) => {
     const htmlAdd = `<div class="card">
-      <img src="${dogImage}" alt="Perro" />
+      <img src="${dog.perricoImg}" alt="Perro" />
+      <h1>${dog.perricoName}</h1>
       <br />
       <p>❤️ 🤮</p>
       <button>Preciosísimo</button> <button>Feísisimo</button>
     </div>`;
 
-    console.log('innerHtml posición', index, dogList.innerHTML);
+    //console.log('innerHtml posición', index, dogList.innerHTML);
 
     dogList.innerHTML += htmlAdd;
   });
@@ -22,12 +22,73 @@ function renderPerricoArray() {
 
 const addPerrico = async () => {
   const perricoImg = await getRandomDogImage();
-  perricosArray.push(perricoImg);
-  renderPerricoArray();
+
+  const randomNumber = Math.floor(Math.random() * 3)
+  const perricoName = namePerricosArray[randomNumber]
+
+  perricosArray.push({perricoImg, perricoName});
+
+  console.log(perricoImg)
+  console.log(perricoName)
+  console.log(perricosArray)
+  renderPerricoArray(perricosArray);
 };
 
-renderPerricoArray();
+
+const addCincoPerrico = async () => {
+  let cont = 0
+
+  while(cont<=4){
+
+    const perricoImg = await getRandomDogImage();
+
+    const randomNumber = Math.floor(Math.random() * 3)
+    const perricoName = namePerricosArray[randomNumber]
+
+    perricosArray.push({perricoImg, perricoName});
+
+    cont ++
+  }
+  
+  renderPerricoArray(perricosArray);
+};
+
+renderPerricoArray(perricosArray);
+
+selectedPerrico=null
+
+function onlyOnePerrico(name){
+  console.log(name)
+  
+  if(selectedPerrico === name){
+    selectedPerrico = null
+    renderPerricoArray(perricosArray)
+  }else{
+    selectedPerrico = name
+    listOnlyMierdon = perricosArray.filter(function(perrico){
+      return perrico.perricoName.includes(name)
+    })
+    console.log(listOnlyMierdon)
+    renderPerricoArray(listOnlyMierdon)
+  }
+  
+}
 
 document.querySelector('#add-1-perrico').addEventListener('click', function () {
   addPerrico();
 });
+
+document.querySelector('#add-5-perrico').addEventListener('click', function () {
+  addCincoPerrico();
+});
+
+document.querySelector('#perrico-lucas').addEventListener('click', function () {
+  onlyOnePerrico(this.innerText);
+});
+document.querySelector('#perrico-mierdon').addEventListener('click', function () {
+  onlyOnePerrico(this.innerText);
+});
+document.querySelector('#perrico-ramon').addEventListener('click', function () {
+  onlyOnePerrico(this.innerText);
+});
+
